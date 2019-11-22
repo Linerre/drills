a6abfdb701bed7d696e9a398dd47e6b44b1c75ab
Linerre <leon.errelin@outlook.com>
2019-11-23 03:42:10 +0800

    keep learning to read error message

--- chapter1/ex1-2.c
+++ chapter1/ex1-2.c
@@ -1,21 +1,17 @@
 #include <stdio.h>
 main()
 {
-  printf("hello, world\n");
+  printf("hello, world\c\n");
   // return 0; nothing special if commented out
 }
 
-// commenting out the first line will cause the following message:
+// got the following warning:
 /*
-ex1-1.c:2:1: warning: return type defaults to ‘int’ [-Wimplicit-int]
+ex1-2.c:2:1: warning: return type defaults to ‘int’ [-Wimplicit-int]
     2 | main()
       | ^~~~
-ex1-1.c: In function ‘main’:
-ex1-1.c:4:3: warning: implicit declaration of function ‘printf’ [-Wimplicit-function-declaration]
-    4 |   printf("hello, world\n");
-      |   ^~~~~~
-ex1-1.c:4:3: warning: incompatible implicit declaration of built-in function ‘printf’
-ex1-1.c:1:1: note: include ‘<stdio.h>’ or provide a declaration of ‘printf’
-  +++ |+#include <stdio.h>
-    1 | // #include <stdio.h>
+ex1-2.c: In function ‘main’:
+ex1-2.c:4:10: warning: unknown escape sequence: '\c'
+    4 |   printf("hello, world\c\n");
+      |          ^~~~~~~~~~~~~~~~~~
 */
