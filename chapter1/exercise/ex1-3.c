fb32b69b3987f4bf2743faec83e617dcab1774c8
Linerre <leon.errelin@outlook.com>
2019-11-25 23:17:44 +0800

    finish Exercise 1-3

--- chapter1/exercise/ex1-3.c
+++ chapter1/exercise/ex1-3.c
@@ -13,7 +13,8 @@ main()
   fahr = lower;
   while (fahr <= upper) {
     celsius = (5.0/9.0) * (fahr -32.0);
-    printf("%3.0f\t%6.1f\n", fahr, celsius);
+    printf("Fahrenheit\tCelsiuss\n");
+    printf("%3.0f\t\t%6.1f\n", fahr, celsius);
     fahr = fahr + step;
   }
 }
