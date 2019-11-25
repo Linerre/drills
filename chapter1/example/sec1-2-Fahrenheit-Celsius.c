9630688b1eb4277f531310a14e6d36b6574902c6
Linerre <leon.errelin@outlook.com>
2019-11-25 22:22:48 +0800

    adjust floating-point accordingly throughout

--- chapter1/example/sec1-2-Fahrenheit-Celsius.c
+++ chapter1/example/sec1-2-Fahrenheit-Celsius.c
@@ -12,8 +12,8 @@ main()
 
   fahr = lower;
   while (fahr <= upper) {
-    celsius = 5 * (fahr -32) / 9;
-    printf("%d\t%d\n", fahr, celsius);
+    celsius = (5.0/9.0) * (fahr -32.0);
+    printf("%3.0f\t%6.1f\n", fahr, celsius);
     fahr = fahr + step;
   }
 }
