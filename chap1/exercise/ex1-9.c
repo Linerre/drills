b76346015aa3701ab84339d65fa35b062f33f536
Linerre <leon.errelin@outlook.com>
2020-08-25 19:28:27 +0800

    add solution from answer book

--- chap1/exercise/ex1-9.c
+++ chap1/exercise/ex1-9.c
@@ -23,3 +23,23 @@ main()
     lastc = c;
   }
 }
+
+/* a more concise implementaion from the Answer Book */
+#include <stdio.h>
+
+#define NONBLANK 'a'
+
+main()
+{
+  int c, lastc;
+
+  lastc = NONBLANK;
+  while ((c = getchar()) != EOF) {
+    if (c != ' ')
+      putchar(c);
+    if (c == ' ')
+      if (lastc != ' ')
+        putchar(c);
+    lastc = c;
+  }
+}
