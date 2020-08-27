b3e7b201ecb3bc9d7e58ad6bf54cc457f2547a52
Linerre <leon.errelin@outlook.com>
2020-08-27 21:32:57 +0800

    almost-there version

--- chap1/exercise/ex1-12.c
+++ chap1/exercise/ex1-12.c
@@ -5,19 +5,26 @@
 
 /* count lines, words, and characters in input */
 main() {
-  int c, state;
+  int c, state, lastc;
 
-  // state = OUT;
-  // nl=nw=nc=0;
+  state = OUT;
   while((c=getchar()) != EOF) {
-    if (c == ' ' || c == '\t') {
-      // state = OUT;
-      printf("\n");
+    if (c == ' ' || c == '\t' || c == '\n') {
+      state = OUT;
+      // printf("\n");
     }
-    else {
-      // state = IN;
+    if (state == OUT) {
+      if (lastc == ' ' || lastc == '\t' || lastc == '\n') {
+        printf("\n");
+        putchar(c);
+        state = IN;
+      }
+    }
+    else
       putchar(c);
-    }
+  
+  lastc = c; 
   }
+  
   // printf("newline: %d; new word: %d; new character: %d\n", nl, nw, nc);
 }
