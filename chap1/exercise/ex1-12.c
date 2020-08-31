0885a252be993f1c32fafe62e16f52043cb29585
Linerre <leon.errelin@outlook.com>
2020-08-31 19:52:17 +0800

    ex-12 done

--- chap1/exercise/ex1-12.c
+++ chap1/exercise/ex1-12.c
@@ -10,21 +10,14 @@ main() {
   state = OUT;
   while((c=getchar()) != EOF) {
     if (c == ' ' || c == '\t' || c == '\n') {
-      state = OUT;
-      // printf("\n");
-    }
-    if (state == OUT) {
-      if (lastc == ' ' || lastc == '\t' || lastc == '\n') {
-        printf("\n");
+      if (state == IN) {
+       putchar('\n');
+       state = OUT;
+      }   
+    } else if (state == OUT) {
+       state = IN;
+       putchar(c);
+    } else
         putchar(c);
-        state = IN;
-      }
-    }
-    else
-      putchar(c);
-  
-  lastc = c; 
   }
-  
-  // printf("newline: %d; new word: %d; new character: %d\n", nl, nw, nc);
 }
