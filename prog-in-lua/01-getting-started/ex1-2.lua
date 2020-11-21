f8d788ed541234eff9840c92ffafca94dcc01ede
Linerre <leon.errelin@outlook.com>
2020-11-21 14:22:29 +0800

    ex 1.2

---
+++ prog-in-lua/01-getting-started/ex1-2.lua
@@ -0,0 +1,15 @@
+-- Exercise 1.2 
+-- Which you like?
+-- Date: 2020-11-21
+
+function norm (x, y)
+  return math.sqrt(x^2 + y^2)
+end
+
+function twice (x)
+  return 2.0 * x
+end
+
+
+-- I like the "-l" otpion when I am going to lauch lua
+-- while I prefer "dofile()" when I am already in an interactive session
